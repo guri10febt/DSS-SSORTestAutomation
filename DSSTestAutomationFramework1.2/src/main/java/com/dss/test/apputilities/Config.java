c4e6263bbdc14d718404fa9a090aa1361b0730ba
guri10febt <guri10febt@gmail.com>
2017-01-30 21:12:00 +0530

    Update Config.java

--- DSSTestAutomationFramework1.2/src/main/java/com/dss/test/apputilities/Config.java
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/apputilities/Config.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.dss.test.coreutilities.Log;
-
+// Testing the new conflict
 public class Config {
 	public WebDriver driver;
 	
